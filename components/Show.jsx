02eb6d296857399127d61cbef6e14916a69f546b
vishalicious213 <vishalicious213@gmail.com>
2023-06-23 00:28:41 -0700

    moved .gallery background color to body. removed .show-map from Show

--- components/Show.jsx
+++ components/Show.jsx
@@ -6,7 +6,6 @@ export default function Show(props) {
             <div className="show-image" style={{backgroundImage: `url(${props.pics[0]})`}}></div>
             <div className="show-details">
                 <p className="show-location"><a href={props.map} target="blank"><img src={pin} /></a> {props.location}</p>
-                {/* <p className="show-map">View on Google Maps</p> */}
                 <h2 className="show-name">{props.name}</h2>
                 <p className="show-date">{props.date}</p>
                 <p className="show-desc">{props.desc}</p>
