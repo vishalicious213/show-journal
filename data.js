0da049fffc13f6d016ae0c902ff643f21f812777
vishalicious213 <vishalicious213@gmail.com>
2023-06-22 21:45:54 -0700

    updated data.js. added 3 placeholder images

--- data.js
+++ data.js
@@ -1,11 +1,29 @@
 export default [
     {
         id: 0,
-        show: "show name",
+        show: "show name 1",
         location: "show location",
         map: "google map link",
         date: "2022 Jan 01",
         desc: "show desc",
-        pics: []
-    }
+        pics: ["show-1.jpg", "show-2.jpg", "show-3.jpg"]
+    },
+    {
+        id: 1,
+        show: "show name 2",
+        location: "show location",
+        map: "google map link",
+        date: "2022 Feb 01",
+        desc: "show desc",
+        pics: ["show-1.jpg", "show-2.jpg", "show-3.jpg"]
+    },
+    {
+        id: 2,
+        show: "show name 3",
+        location: "show location",
+        map: "google map link",
+        date: "2022 Mar 01",
+        desc: "show desc",
+        pics: ["show-1.jpg", "show-2.jpg", "show-3.jpg"]
+    },
 ]
