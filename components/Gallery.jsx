1443bf4002f633d5362af3d0b09dbb48e3b8ecb6
vishalicious213 <vishalicious213@gmail.com>
2023-06-22 22:25:39 -0700

    drafted /components/Show.jsx. imported to Gallery. Rendering via App

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,7 +1,10 @@
+import Show
+ from "./Show"
 export default function Gallery() {
     return (
         <section className="gallery container">
             <h2>Gallery</h2>
+            <Show />
         </section>
     )
 }
---
+++ components/Show.jsx
@@ -0,0 +1,5 @@
+export default function Show() {
+    return (
+        <h2>Show</h2>
+    )
+}
