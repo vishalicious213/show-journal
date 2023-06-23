d2d73f6d9c7cb0fcf61a934b83f7577e81c41014
vishalicious213 <vishalicious213@gmail.com>
2023-06-22 22:39:01 -0700

    passing a show from data.js to Show from Gallery and rendering it

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,10 +1,12 @@
-import Show
- from "./Show"
+import data from "../data"
+import Show from "./Show"
+
 export default function Gallery() {
+    console.log(data[0])
     return (
         <section className="gallery container">
             <h2>Gallery</h2>
-            <Show />
+            <Show {...data[0]} />
         </section>
     )
 }
