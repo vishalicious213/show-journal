5b23a0212b5c7736d34ce45cd1dd940f258fd8b8
vishalicious213 <vishalicious213@gmail.com>
2023-06-23 15:00:17 -0700

    added traitrs/hallows show details to data.js

--- data.js
+++ data.js
@@ -11,11 +11,11 @@ export default [
     // },
     {
         id: 2,
-        name: "Show Name 3",
-        location: "show location",
+        name: "TRAITRS / HALLOWS",
+        location: "Catch One",
         map: "https://reddit.com",
-        date: "2022 Mar 01",
-        desc: "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        date: "2022 Jul 21",
+        desc: "At TRAITRS  and HALLOWS  show with Sue. Traitrs was more than I expected. Live, they're somewhere between The Cure and Jesu. Fantastic show.",
         full: ``,
         pics: ["show-3.jpg", "show-1.jpg", "show-2.jpg"]
     },
