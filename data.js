1422fe01289ca75b4d20efb52b3d24b3f6d44d27
vishalicious213 <vishalicious213@gmail.com>
2023-06-22 21:42:58 -0700

    drafted data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        id: 0,
+        show: "show name",
+        location: "show location",
+        map: "google map link",
+        date: "2022 Jan 01",
+        desc: "show desc",
+        pics: []
+    }
+]
