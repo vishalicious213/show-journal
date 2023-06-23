02cdba06e373a754b1c006b910b201ac7f8eb598
vishalicious213 <vishalicious213@gmail.com>
2023-06-22 22:33:23 -0700

    updated Show.jsx with props and JSX elements

--- components/Show.jsx
+++ components/Show.jsx
@@ -1,5 +1,22 @@
-export default function Show() {
+export default function Show(props) {
     return (
-        <h2>Show</h2>
+        <div className="show">
+            <img src={props.pics[0]} alt={props.name} />
+            <div className="show-details">
+                <p className="show-location">{props.location}</p>
+                <p className="show-map">{props.map}</p>
+                <h2 className="show-name">{props.name}</h2>
+                <p className="show-date">{props.date}</p>
+                <p className="show-desc">{props.desc}</p>
+            </div>
+        </div>
     )
 }
+
+// id: 0,
+// name: "show name 1",
+// location: "show location",
+// map: "google map link",
+// date: "2022 Jan 01",
+// desc: "show desc",
+// pics: ["show-1.jpg", "show-2.jpg", "show-3.jpg"]
--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export default [
     {
         id: 0,
-        show: "show name 1",
+        name: "show name 1",
         location: "show location",
         map: "google map link",
         date: "2022 Jan 01",
@@ -10,7 +10,7 @@ export default [
     },
     {
         id: 1,
-        show: "show name 2",
+        name: "show name 2",
         location: "show location",
         map: "google map link",
         date: "2022 Feb 01",
@@ -19,7 +19,7 @@ export default [
     },
     {
         id: 2,
-        show: "show name 3",
+        name: "show name 3",
         location: "show location",
         map: "google map link",
         date: "2022 Mar 01",
