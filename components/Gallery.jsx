2752eb60370e9fa0a26bbea9e4327d70e7e4aabd
vishalicious213 <vishalicious213@gmail.com>
2023-06-23 10:18:30 -0700

    added #article to index.html. setting its innerText when show image is clicked

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -2,10 +2,14 @@ import data from "../data"
 import Show from "./Show"
 
 export default function Gallery() {
+    const article = document. getElementById("article")
+    console.log(article)
+
     document.addEventListener("click", function(e) {
         if (e.target.dataset.id) {
             console.log("clicked", e.target.dataset.id)
             console.log(data[e.target.dataset.id].desc)
+            article.innerText="ARTICLE"
         }
     })
 
