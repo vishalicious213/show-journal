9fba515cadbe75ab368657841b8314b3a1714baf
vishalicious213 <vishalicious213@gmail.com>
2023-06-23 02:30:08 -0700

    cleaned up .show-image code

--- components/Article.jsx
+++ components/Article.jsx
@@ -1,5 +1,6 @@
 export default function Article() {
-    document.querySelector(".gallery").classList.add("hide")
+    console.log("Article")
+    // document.querySelector(".gallery").classList.add("hide")
     return (
         <h1>ARTICLE</h1>
     )
--- components/Show.jsx
+++ components/Show.jsx
@@ -3,7 +3,11 @@ import pin from "../public/pin.png"
 export default function Show(props) {
     return (
         <div className="show">
-            <div className="show-image" data-id={props.id} style={{backgroundImage: `url(${props.pics[0]})`}}></div>
+            <div 
+                className="show-image" 
+                data-id={props.id} 
+                style={{backgroundImage: `url(${props.pics[0]})`}}>
+            </div>
             <div className="show-details">
                 <p className="show-location"><a href={props.map} target="blank"><img src={pin} /></a> {props.location}</p>
                 <h2 className="show-name">{props.name}</h2>
