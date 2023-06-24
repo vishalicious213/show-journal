e2cbb29f33aed40bdab413601ca52ff60dd33243
vishalicious213 <vishalicious213@gmail.com>
2023-06-24 01:39:19 -0700

    drafted & styled Footer. rendered it in App

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Gallery from "./components/Gallery"
+import Footer from "./components/Footer"
 
 export default function App() {
     return (
         <>
             <Header />
             <Gallery />
+            <Footer />
         </>
     )
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>...All flesh entwined...</p>
+        </footer>
+    )
+}
